529d8d7606a03c7c8f12ecce3e7cb93e6cabc2e3
Kelsey Bosell <kimbosell@Kelseys-MacBook-Pro-3.local>
2015-03-20 09:27:19 -0700

    update 04

--- js/cordova.js
+++ js/cordova.js
@@ -1,1 +1,3 @@
 navigator.splashscreen.hide();
+
+navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
--- js/formFill.js
+++ js/formFill.js
@@ -451,3 +451,7 @@ function changePage(loadPage){
 		break;
 	}
 }
+
+navigator.splashscreen.hide();
+
+navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
