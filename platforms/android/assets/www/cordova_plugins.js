35f8f6a9f518b1ff154b16b59f2b4ae2572e1376
Kelsey Bosell <kimbosell@Kelseys-MacBook-Pro-3.local>
2015-03-20 21:25:37 -0700

    update 12

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.insomnia/www/Insomnia.js",
+        "id": "nl.x-services.plugins.insomnia.Insomnia",
+        "clobbers": [
+            "window.plugins.insomnia"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "org.apache.cordova.vibration": "0.3.13",
+    "nl.x-services.plugins.insomnia": "4.0.1"
+}
+// BOTTOM OF METADATA
+});
